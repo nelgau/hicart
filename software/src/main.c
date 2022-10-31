483ee910c910b7ecc769f2e2c39f7c3d1f2a57cd
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-10-31 03:01:48 -0700

    soft: Basic video decoding

--- software/src/main.c
+++ software/src/main.c
@@ -1,26 +1,46 @@
-#include <stdio.h>
-
 #include <libdragon.h>
+
+#define NUM_DISPLAY 4
 
 int main(void)
 {
-    console_init();
-
+    controller_init();
+    debug_init_isviewer();
     debug_init_usblog();
-    console_set_debug(true);
 
+    display_init(RESOLUTION_320x240, DEPTH_32_BPP, NUM_DISPLAY, GAMMA_NONE, ANTIALIAS_OFF);
     dfs_init(DFS_DEFAULT_LOCATION);
+    rdp_init();
 
-    FILE *fp = fopen("rom:/data.txt", "r");
-	if (fp) {
-	    fseek(fp, 0, SEEK_END);
-	    int size = ftell(fp);
-	    rewind(fp);
+    mpeg2_t mp2;
+    mpeg2_open(&mp2, "rom:/2013-03-23.bDO3hZYHvwz.m1v");
 
-	    printf("Size: %d\n", size);
-    } else {
-	    printf("Error opening file\n");
+    float fps = mpeg2_get_framerate(&mp2);
+    throttle_init(fps, 0, 8);
+
+    int nframes = 0;
+    display_context_t disp = 0;
+
+    while (1) {
+        if (!mpeg2_next_frame(&mp2))
+            break;
+
+        RSP_WAIT_LOOP(500) {
+            disp = display_lock();
+            if (disp) break;
+        }
+
+        rdp_attach(disp);
+        mpeg2_draw_frame(&mp2, disp);
+        rdp_detach_show(disp);
+
+        nframes++;
+
+        int ret = throttle_wait();
+        if (ret < 0) {
+            debugf("videoplayer: frame %d too slow (%d Kcycles)\n", nframes, -ret);
+        }
+
+        rspq_wait();
     }
-
-    while(1) {}
 }
