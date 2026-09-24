3e63c3eaf6496d3dd063f2b35e9b49dd8de339f5
Nelson Gauthier <nelson.gauthier@gmail.com>
2026-09-24 15:10:26 -0700

    gateware: Supporting mapping with data_width > granularity

--- gateware/tests/soc/test_wishbone.py
+++ gateware/tests/soc/test_wishbone.py
@@ -1,8 +1,9 @@
 from amaranth.sim import *
+from amaranth.utils import exact_log2
 from amaranth_soc import wishbone
 from amaranth_soc.memory import MemoryMap
 
-from hicart.soc.wishbone import DownConverter
+from hicart.soc.wishbone import DownConverter, WindowMapper
 from hicart.sim.behavioral.wishbone import WishboneInitiatorDriver
 from hicart.sim.behavioral.wishbone import WishboneTargetResponder
 from hicart.sim.testcase import MultiProcessTestCase
@@ -32,3 +33,32 @@ class DownConverterTest(MultiProcessTestCase):
             sim.add_clock(1.0 / 100e6, domain="sync")
             sim.add_process(sub_process)
             sim.add_testbench(intr_testbench)
+
+
+class WindowMapperTest(MultiProcessTestCase):
+
+    def test_narrow(self):
+        sub_bus = wishbone.Interface(addr_width=24, data_width=8)
+        sub_bus.memory_map = MemoryMap(addr_width=24, data_width=8)
+
+        dut = WindowMapper(sub_bus, addr_width=23, base_addr=0x800000)
+
+        async def testbench(ctx):
+            ctx.set(dut.bus.adr, 0x4000)
+            assert ctx.get(sub_bus.adr) == 0x804000
+
+        with self.simulate(dut) as sim:
+            sim.add_testbench(testbench)
+
+    def test_wide(self):
+        sub_bus = wishbone.Interface(addr_width=23, data_width=16, granularity=8)
+        sub_bus.memory_map = MemoryMap(addr_width=24, data_width=8)
+
+        dut = WindowMapper(sub_bus, addr_width=22, base_addr=0x800000)
+
+        async def testbench(ctx):
+            ctx.set(dut.bus.adr, 0x2000)
+            assert ctx.get(sub_bus.adr) == 0x402000
+
+        with self.simulate(dut) as sim:
+            sim.add_testbench(testbench)
